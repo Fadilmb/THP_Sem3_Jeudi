a0b46a38d68cbf06a870efdd44f230ffd27e3926
Fadilmb <fadil.boodoo@etu.upmc.fr>
2017-07-20 22:19:09 +0200

    done

--- Board.rb
+++ Board.rb
@@ -3,7 +3,6 @@ class Board
   include Enumerable
   #TO DO : la classe a 1 attr_accessor, une array qui contient les BoardCases
   attr_accessor :BoardCases_contain
-#  @@nombre_de_tour = 0
 
   def initialize
     @BoardCases_contain = []
@@ -13,67 +12,63 @@ class Board
     for i in (0..8)
     	@BoardCases_contain << BoardCase.new(i+1)
     end
-#
   end
 
   def display
   #TO DO : afficher le plateau
-  puts "#{@BoardCases_contain[0]} #{@BoardCases_contain[1]} #{@BoardCases_contain[2]}"
-  puts "#{@BoardCases_contain[3]} #{@BoardCases_contain[4]} #{@BoardCases_contain[5]}"
-  puts "#{@BoardCases_contain[6]} #{@BoardCases_contain[7]} #{@BoardCases_contain[8]}"
+    puts "#{@BoardCases_contain[0]} #{@BoardCases_contain[1]} #{@BoardCases_contain[2]}"
+    puts "#{@BoardCases_contain[3]} #{@BoardCases_contain[4]} #{@BoardCases_contain[5]}"
+    puts "#{@BoardCases_contain[6]} #{@BoardCases_contain[7]} #{@BoardCases_contain[8]}"
   end
 
-  def play (nplayer)
+  def play (player)
     #TO DO : une méthode qui change la BoardCase jouée en fonction de la valeur du joueur (X, ou O)
     puts "à toi de jouer!
-    entre la numéro de la case que tu veux jouer"
+    entre la numéro de la case que tu veux jouer (entre 1 et 9)"
     case_number_played = gets.chomp().to_i
-#    @@nombre_de_tour +=1
-#    if @@nombre_de_tour %2 == 0
-    if nplayer == "X"
+    if player == "X"
     	@BoardCases_contain[case_number_played-1].value = "X"
     else
     	@BoardCases_contain[case_number_played-1].value = "O"
     end
   end
 
-  def victory?
+  def victory?(player_name)
     #TO DO : qui gagne ?
-      if "{#{@BoardCases_contain[0]}#{@BoardCases_contain[1]}#{@BoardCases_contain[1]}" == "XXX" || "#{@BoardCases_contain[0]}#{@BoardCases_contain[1]}#{@BoardCases_contain[1]}" == "OOO"
-       puts "You win!"
-       display
-       abort
-      elsif "#{@BoardCases_contain[3]}#{@BoardCases_contain[4]}#{@BoardCases_contain[5]}" == "XXX" || "#{@BoardCases_contain[4]}#{@BoardCases_contain[5]}#{@BoardCases_contain[6]}" == "OOO"
-       puts "You win!"
-       display
+    if "{#{@BoardCases_contain[0]}#{@BoardCases_contain[1]}#{@BoardCases_contain[1]}" == "XXX" || "#{@BoardCases_contain[0]}#{@BoardCases_contain[1]}#{@BoardCases_contain[1]}" == "OOO"
+      puts "#{player_name} win!"
+      display
       abort
-      elsif "#{@BoardCases_contain[6]}#{@BoardCases_contain[7]}#{@BoardCases_contain[8]}" == "XXX" || "#{@BoardCases_contain[6]}#{@BoardCases_contain[7]}#{@BoardCases_contain[8]}" == "OOO"
-       puts "You win!"
-       display
+    elsif "#{@BoardCases_contain[3]}#{@BoardCases_contain[4]}#{@BoardCases_contain[5]}" == "XXX" || "#{@BoardCases_contain[4]}#{@BoardCases_contain[5]}#{@BoardCases_contain[6]}" == "OOO"
+      puts "#{player_name} win!"
+      display
       abort
-      elsif "#{@BoardCases_contain[0]}#{@BoardCases_contain[3]}#{@BoardCases_contain[6]}" == "XXX" || "#{@BoardCases_contain[0]} + #{@BoardCases_contain[3]} + #{@BoardCases_contain[6]}" == "OOO"
-       puts "You win!"
-       display
+    elsif "#{@BoardCases_contain[6]}#{@BoardCases_contain[7]}#{@BoardCases_contain[8]}" == "XXX" || "#{@BoardCases_contain[6]}#{@BoardCases_contain[7]}#{@BoardCases_contain[8]}" == "OOO"
+      puts "#{player_name} win!"
+      display
       abort
-      elsif "#{@BoardCases_contain[1]}#{@BoardCases_contain[4]}#{@BoardCases_contain[7]}" == "XXX" || "#{@BoardCases_contain[1]}#{@BoardCases_contain[4]}#{@BoardCases_contain[7]}" == "OOO"
-       puts "You win!"
-       display
+    elsif "#{@BoardCases_contain[0]}#{@BoardCases_contain[3]}#{@BoardCases_contain[6]}" == "XXX" || "#{@BoardCases_contain[0]} + #{@BoardCases_contain[3]} + #{@BoardCases_contain[6]}" == "OOO"
+      puts "#{player_name} win!"
+      display
       abort
-      elsif "#{@BoardCases_contain[2]}#{@BoardCases_contain[5]}#{@BoardCases_contain[8]}" == "XXX" || "#{@BoardCases_contain[2]}#{@BoardCases_contain[5]}#{@BoardCases_contain[8]}" == "OOO"
-       puts "You win!"
-       display
-       abort
-      elsif "#{@BoardCases_contain[0]}#{@BoardCases_contain[4]}#{@BoardCases_contain[8]}" == "XXX" || "#{@BoardCases_contain[0]}#{@BoardCases_contain[4]}#{@BoardCases_contain[8]}" == "OOO"
-       puts "You win!"
-       display
-       abort
-      elsif "#{@BoardCases_contain[2]}#{@BoardCases_contain[4]}#{@BoardCases_contain[6]}" == "XXX" || "#{@BoardCases_contain[2]}#{@BoardCases_contain[4]}#{@BoardCases_contain[6]}" == "OOO"
-       puts "You win!"
-       display
-       abort
-      else
-      end
+    elsif "#{@BoardCases_contain[1]}#{@BoardCases_contain[4]}#{@BoardCases_contain[7]}" == "XXX" || "#{@BoardCases_contain[1]}#{@BoardCases_contain[4]}#{@BoardCases_contain[7]}" == "OOO"
+      puts "#{player_name} win!"
+      display
+      abort
+    elsif "#{@BoardCases_contain[2]}#{@BoardCases_contain[5]}#{@BoardCases_contain[8]}" == "XXX" || "#{@BoardCases_contain[2]}#{@BoardCases_contain[5]}#{@BoardCases_contain[8]}" == "OOO"
+      puts "#{player_name} win!"
+      display
+      abort
+    elsif "#{@BoardCases_contain[0]}#{@BoardCases_contain[4]}#{@BoardCases_contain[8]}" == "XXX" || "#{@BoardCases_contain[0]}#{@BoardCases_contain[4]}#{@BoardCases_contain[8]}" == "OOO"
+      puts "#{player_name} win!"
+      display
+      abort
+    elsif "#{@BoardCases_contain[2]}#{@BoardCases_contain[4]}#{@BoardCases_contain[6]}" == "XXX" || "#{@BoardCases_contain[2]}#{@BoardCases_contain[4]}#{@BoardCases_contain[6]}" == "OOO"
+      puts "#{player_name} win!"
+      display
+      abort
+    else
+    end
   end
-
 end
 
--- Tic_Ta_Toe.rb
+++ Tic_Ta_Toe.rb
@@ -1,46 +1,46 @@
 require './Player'
 require './Board'
+
 class Game
   def initialize
     #TO DO : créé 2 joueurs, créé un board
     puts "name of the player1 ?"
-    name_of_player_1 = gets.chomp
+    name_of_player_1 = gets.chomp.to_s
     @player1 = Player.new(name_of_player_1,"X")
     puts "name of the player2 ?"
-    name_of_player_2 = gets.chomp
+    name_of_player_2 = gets.chomp.to_s
     @player2 = Player.new(name_of_player_2, "O")
-    @io = Board.new
+    @Board = Board.new
     @@turn_number = 0
     go
   end
 
   def go
     # TO DO : lance la partie
-
     while true
-    	turn
+    	@@turn_number +=1
     	if @@turn_number < 10
+    		turn
     	else
-    	 puts "Match nul"
-       display
-    	 abort
+    		puts "Match nul"
+      	@Board.display
+    		abort
     	end
     end
   end
 
   def turn
     #TO DO : affiche le plateau, demande au joueur il joue quoi, vérifie si un joueur a gagné, passe au joueur suivant si la partie n'est pas finie
-  	@@turn_number +=1
   	if @@turn_number % 2 == 0
-  	@io.display
-  	@io.play("O")
-  	@io.victory?
-  	@player2.win +=1
+  		@Board.display
+  		@Board.play("O")
+  		@Board.victory?(@player2.name)
+  		@player2.win +=1
   	else
-  	@io.display
-  	@io.play("X")
-  	@io.victory?
-  	@player1.win +=1
+  		@Board.display
+  		@Board.play("X")
+  		@Board.victory?(@player1.name)
+  		@player1.win +=1
   	end
   end
 end
